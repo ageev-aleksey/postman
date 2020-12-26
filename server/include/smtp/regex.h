d638fea819945c4c30158d2bf2c706bbf8c11ad1
Ageev Aleksey <ageevav@student.bmstu.ru>
2020-12-26 07:54:17 +0300

    Поправлен отчет

--- server/include/smtp/regex.h
+++ server/include/smtp/regex.h
@@ -1,13 +1,9 @@
 #define RE_DOMAIN "([[:alnum:]]+\\.)+[[:alnum:]]+"
 #define RE_IPv4 "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
-#define RE_IPv6 " "
-#define RE_GENERAL_ADDRESS_LITERAL " "
 #define RE_ADDRESS_LITERAL  "\\[" RE_IPv4 "\\]"
-//#define RE_HELLO_ADDR "(" RE_DOMAIN ")|(" RE_ADDRESS_LITERAL ")"
 #define RE_DOMAIN_LITERAL "<" RE_DOMAIN ">"
 #define RE_AT_DOMAIN "@" RE_DOMAIN
-#define RE_MAILBOX "[[:alnum:]]+" RE_AT_DOMAIN // TODO (ageev) если имя содеожит '_' то регулярное выражение не сопоставляется
-//#define RE_PATH "<((" RE_AT_DOMAIN ",)*("RE_AT_DOMAIN":)){0,1}" RE_MAIL_BOX ">"
+#define RE_MAILBOX "[[:alnum:]]+" RE_AT_DOMAIN
 #define RE_ROUTE_PATH "((" RE_AT_DOMAIN "[[:space:]]*,[[:space:]]*)*" RE_AT_DOMAIN "[[:space:]]*:[[:space:]]*){0,1}"
 #define RE_PATH "<" RE_ROUTE_PATH RE_MAILBOX ">"
 #define RE_SERVER_NAME "(" RE_ADDRESS_LITERAL ")|(" RE_DOMAIN_LITERAL ")";
